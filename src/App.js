4e19e25a2246934201de98999b62ad47131e278f
Jake <jakerfilut@gmail.com>
2022-01-03 20:53:17 -0700

    Departments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Navbar from "./components/NavBar/Navbar";
 import Product from "./components/Product/Product";
+import Slideshow from "./components/Slideshow/Slideshow";
+import Department from "./components/Department/Department";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Slideshow />
+      <Department />
       <Product />
     </div>
   );
