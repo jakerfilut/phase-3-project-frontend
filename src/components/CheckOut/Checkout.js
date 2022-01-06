dabc6ba8efecc753cf2680ac4c99781830f93f4b
Jake <jakerfilut@gmail.com>
2022-01-05 22:09:28 -0700

    cleaned up

--- src/components/CheckOut/Checkout.js
+++ src/components/CheckOut/Checkout.js
@@ -1,29 +1,6 @@
 import { React, useState, useEffect } from "react";
 
 function Checkout({ cart, handleDelete }) {
-  // const [cart, setCart] = useState([]);
-  // const [deletes, setDeletes] = useState(false);
-
-  // useEffect(() => {
-  //   fetch("http://localhost:9292/order_items")
-  //     .then((res) => res.json())
-  //     .then(setCart);
-  // }, [deletes]);
-  // console.log(cart.length);
-  // const handleDelete = (item) => {
-  //   fetch(`http://localhost:9292/order_items/${item.id}`, {
-  //     method: "DELETE",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setCart(cart.filter((item) => item.id !== data.id));
-  //       setDeletes(!deletes);
-  //     });
-  // };
-
   const mapCart = cart.map((item) => {
     return (
       <p>
